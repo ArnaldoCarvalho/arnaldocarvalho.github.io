62c8f83c302a46695c5f881362be22fa2c0bffdb
Arnaldo Carvalho <108664667+ArnaldoCarvalho@users.noreply.github.com>
2026-03-11 18:35:35 +0000

    Delete SAD_WebSite directory
    WEB Site não é mais preciso. disciplina encerrada

--- SAD_WebSite/static/script.js
+++
@@ -1,6 +0,0 @@
-function mostrarMensagem() {
-    alert("Você clicou no botão!");
-    const btn = document.querySelector("button");
-    btn.classList.add("clicado");
-    setTimeout(() => btn.classList.remove("clicado"), 400);
-}
